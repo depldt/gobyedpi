7e169ea3290d3fb62cdd95fe19899d375d281dee
depldt <depldt@gmail.com>
2026-04-16 14:28:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -66,7 +66,6 @@ cat > Dockerfile << 'EOF'
 FROM docker.io/alpine:latest
 RUN apk add --no-cache ca-certificates openssl tzdata
 RUN mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-RUN adduser -D -s /bin/sh gostuser
 RUN mkdir -p /etc/gost /etc/byedpi /usr/local/bin
 
 # Копирование файлов
